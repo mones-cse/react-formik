9e58b6e28636cf9497c0146abbd61781b2120fe4
mones <mones.cse@gmail.com>
2021-07-25 13:59:25 +0600

    basic formik usage

--- src/components/YoutubeForm.js
+++ src/components/YoutubeForm.js
@@ -1,17 +1,29 @@
 import React from 'react';
+import {useFormik} from "formik";
 
 const YouTubeForm = () => {
+    const formik = useFormik({
+        initialValues: {
+            name: "",
+            email: "",
+            channel: ""
+        }
+
+    });
+    console.log('Form values',formik.values);
     return (
         <div>
             <form action="">
                 <label htmlFor="name">Name</label>
-                <input type="text" id={"name"} name={"name"}/>
+                <input type="text" id={"name"} name={"name"} onChange={formik.handleChange} value={formik.values.name}/>
 
                 <label htmlFor="email">Email</label>
-                <input type="email" name={"email"} id={"email"}/>
+                <input type="email" name={"email"} id={"email"} onChange={formik.handleChange}
+                       value={formik.values.email}/>
 
                 <label htmlFor="channel">Channel</label>
-                <input type="text" id={"channel"} name={"channel"}/>
+                <input type="text" id={"channel"} name={"channel"} onChange={formik.handleChange}
+                       value={formik.values.channel}/>
 
                 <button>Submit</button>
             </form>
